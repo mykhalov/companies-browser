3e347cb96cef3c4827edba548efaa4e38b87c196
Pavlo Mykhalov <git@mykhalov.com>
2022-02-04 17:09:06 +0200

    add datalist

--- components/input.tsx
+++ components/input.tsx
@@ -1,15 +1,24 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { stateContext } from "./state";
 
 const CompanyNameInput: React.FC = () => {
-  const [, setState] = useContext(stateContext);
+  const [state, setState] = useContext(stateContext);
+  const listId = useMemo(() => Math.random().toString().replace("0", "id"), []);
 
   return (
-    <input
-      placeholder="Company name"
-      onChange={(e) => setState((s) => ({ ...s, query: e.target.value }))}
-      className="p-2 shadow-inner"
-    />
+    <>
+      <input
+        placeholder="Company name"
+        onChange={(e) => setState((s) => ({ ...s, query: e.target.value }))}
+        className="p-2 shadow-inner"
+        list={listId}
+      />
+      <datalist id={listId}>
+        {state.companies.map((c) => (
+          <option key={c.name} value={c.name} />
+        ))}
+      </datalist>
+    </>
   );
 };
 
