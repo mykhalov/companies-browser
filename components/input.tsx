2812e5adfcaa8a3604ff033a06f16398099630b4
Pavlo Mykhalov <git@mykhalov.com>
2022-02-04 17:12:44 +0200

    add debounce

--- components/input.tsx
+++ components/input.tsx
@@ -1,15 +1,21 @@
-import { useContext, useMemo } from "react";
+import debounce from "lodash.debounce";
+import { useCallback, useContext, useMemo } from "react";
 import { stateContext } from "./state";
 
 const CompanyNameInput: React.FC = () => {
   const [state, setState] = useContext(stateContext);
   const listId = useMemo(() => Math.random().toString().replace("0", "id"), []);
 
+  const onChange = useCallback(
+    debounce((e) => setState((s) => ({ ...s, query: e.target.value })), 100),
+    []
+  );
+
   return (
     <>
       <input
         placeholder="Company name"
-        onChange={(e) => setState((s) => ({ ...s, query: e.target.value }))}
+        onChange={onChange}
         className="p-2 shadow-inner"
         list={listId}
       />
